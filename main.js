f584b2dfbf131b830e7c6946016d49a317c51bd2
mirmohsin0001 <70447959+mirmohsin0001@users.noreply.github.com>
2023-09-28 13:07:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
-    const button = document.getElementById('#btn');
-    button.addEventListner('click', calculateBMI);    
+    const button = document.getElementById('btn');
+    button.addEventListener('click', calculateBMI);    
 }
 
 
@@ -10,10 +10,11 @@ function calculateBMI() {
     const heightInInches = document.querySelector('#height-inches').value;
     const heightInFeet = document.querySelector('#height-feet').value;
 
-    const heightInMtr = (heightInInches + (heightInFeet * 12)) * 0.0245;
+    const heightInMtr = ((heightInFeet * 12)+ parseInt(heightInInches)) * 0.0245;
+
 
     
-    const bmi = weight / (heightInMtr * heightInMtr);
-    result.innerHTML = ('fsalkdjf sldkjflks jadflkj' + bmi);
+    const bmi = (weight / (heightInMtr * heightInMtr));
+    result.innerHTML = ('Your BMI is: ' + bmi.toFixed(2));
 
 }
